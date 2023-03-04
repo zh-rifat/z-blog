bc8c0915ebdf97b026af61c6fa440e7717b718cc
zh-rifat <zhrifat.00@gmail.com>
2023-03-04 23:17:29 +0600

    v0.5

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api/'
+  baseURL: 'https://zblog-api.adaptable.app/api/'
 });
 
 export default instance;
